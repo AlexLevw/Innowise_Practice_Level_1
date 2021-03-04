a58def6d99bcb22d6bbe207cb027bb1acad001c4
Alex <foxi4554@gmail.com>
2021-03-04 11:22:58 +0300

    feat: change router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "@contexts/AuthContext";
 import styles from "./_App.module.scss";
 import Signup from "./Signup/Signup";
